03c560dc9731b9b96da038b8c425bd8f0a4d99e9
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-10-06 20:24:41 +0530

    UC4 has been created where when statement is added instead of if

--- src/main/kotlin/com/employeewage/CheckAttendance.kt
+++ src/main/kotlin/com/employeewage/CheckAttendance.kt
@@ -1,16 +1,21 @@
 package com.employeewage
 
 fun checkAttendance():Int {
-    val empCheck = (0..2).random()
+    var empCheck:Int = (0..2).random()
 
-    return if(empCheck == Employee.IS_FULL_TIME){
-        println("Employee is present and is full time")
-        Employee.IS_FULL_TIME
-    }else if(empCheck == Employee.IS_PART_TIME){
-        println("Employee is present and is part time")
-        Employee.IS_PART_TIME
-    }else{
-        println("Employee is absent")
-        0
+    return when(empCheck){
+        Employee.IS_FULL_TIME -> {
+            println("Employee is present and is full time")
+            Employee.IS_FULL_TIME
+        }
+        Employee.IS_PART_TIME -> {
+            println("Employee is present and is part time")
+            Employee.IS_PART_TIME
+        }
+        else -> {
+            println("Employee is absent")
+            0
+        }
     }
+
 }
--- src/main/kotlin/com/employeewage/DailyWage.kt
+++ src/main/kotlin/com/employeewage/DailyWage.kt
@@ -2,12 +2,11 @@ package com.employeewage
 
 fun dailyWage(empType:Int):Int {
     var dailyWage = 0
-    dailyWage = if(empType == Employee.IS_FULL_TIME){
-        Employee.WAGE_PER_HOUR * Employee.FULL_TIME_HOUR
-    }else if(empType == Employee.IS_PART_TIME){
-        Employee.WAGE_PER_HOUR * Employee.PART_TIME_HOUR
-    }else{
-        0
+
+    dailyWage = when(empType){
+        Employee.IS_FULL_TIME -> Employee.WAGE_PER_HOUR * Employee.FULL_TIME_HOUR
+        Employee.IS_PART_TIME -> Employee.WAGE_PER_HOUR * Employee.PART_TIME_HOUR
+        else -> 0
     }
     return dailyWage
 }
