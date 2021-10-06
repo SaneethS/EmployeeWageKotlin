49f3f5a36e9245d90ac6c9471dafca05514e4e8f
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-10-06 21:17:16 +0530

    UC6 has been created where the wage is calculated till a particular day and hour in month

--- src/main/kotlin/com/employeewage/DailyWage.kt
+++ src/main/kotlin/com/employeewage/DailyWage.kt
@@ -1,12 +1,11 @@
 package com.employeewage
 
-fun dailyWage(empType:Int):Int {
-    var dailyWage = 0
+fun dailyWage(empType:Int): Pair<Int,Int> {
 
-    dailyWage = when(empType){
-        Employee.IS_FULL_TIME -> Employee.WAGE_PER_HOUR * Employee.FULL_TIME_HOUR
-        Employee.IS_PART_TIME -> Employee.WAGE_PER_HOUR * Employee.PART_TIME_HOUR
-        else -> 0
+    var dailyWage = when(empType){
+        Employee.IS_FULL_TIME -> Pair(Employee.WAGE_PER_HOUR * Employee.FULL_TIME_HOUR, Employee.FULL_TIME_HOUR)
+        Employee.IS_PART_TIME -> Pair(Employee.WAGE_PER_HOUR * Employee.PART_TIME_HOUR, Employee.PART_TIME_HOUR)
+        else -> Pair(0,0)
     }
     return dailyWage
 }
