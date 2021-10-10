c16abf512f0b21aa8ff0ce742686235156ec139e
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-10-10 15:47:45 +0530

    UC14 has been created where wage particular company is retrieved

--- src/main/kotlin/com/employeewage/EmpWageBuilder.kt
+++ src/main/kotlin/com/employeewage/EmpWageBuilder.kt
@@ -3,4 +3,5 @@ package com.employeewage
 interface EmpWageBuilder {
     fun addCompany(companyName: String, empHrs: Int, wagePerHour: Int, hoursInMonth: Int, daysInMonth: Int)
     fun compute()
+    fun getCompany(companyName: String):Int
 }
