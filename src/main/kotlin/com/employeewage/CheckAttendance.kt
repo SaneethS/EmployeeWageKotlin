373f4e3555136eb81a2fc66c5f30fd545bbc687c
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-10-10 11:50:23 +0530

    UC8 has been created where multiple companies are added

--- src/main/kotlin/com/employeewage/CheckAttendance.kt
+++ src/main/kotlin/com/employeewage/CheckAttendance.kt
@@ -1,19 +1,19 @@
 package com.employeewage
 class CheckAttendance {
-    fun checkAttendance():Int {
+    fun checkAttendance(companyName: String):Int {
         var empCheck:Int = (0..2).random()
 
         return when(empCheck){
             Constants.IS_FULL_TIME -> {
-                println("Employee is present and is full time")
+                println("Employee is present and is full time in $companyName")
                 Constants.IS_FULL_TIME
             }
             Constants.IS_PART_TIME -> {
-                println("Employee is present and is part time")
+                println("Employee is present and is part time in $companyName")
                 Constants.IS_PART_TIME
             }
             else -> {
-                println("Employee is absent")
+                println("Employee is absent in $companyName")
                 0
             }
         }
